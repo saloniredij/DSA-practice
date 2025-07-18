a98b61cc9a71120e9297a7508e1e443e5979bb0c
saloniredij <saloni1829@gmail.com>
2025-07-18 01:05:09 -0400

    pushing description

--- src/sliding window/max-vowels-subarray.py
+++ src/sliding window/max-vowels-subarray.py
@@ -1,3 +1,18 @@
+'''
+Given a string s and an integer k, return the maximum number of 
+vowel letters in any substring of s with length k.
+
+Example:
+Input: s = "abciiidef", k = 3
+Output: 3
+substring "iii"
+
+Constraints:
+
+1 <= s.length <= 105
+1 <= k <= s.length
+'''
+
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         i=0
